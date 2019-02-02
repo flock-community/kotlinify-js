f2d29fac62f21a20ffc2983df875fe3eed3139ac
Kasper Peulen <kpeulen@bol.com>
2019-02-02 13:41:44 +0100

    implement Chain with let, also and get methods

--- src/kotlinify.js
+++ src/kotlinify.js
@@ -1,1 +1,28 @@
-export const pipe = value => (...fn) => fn.reduce((acc, cur) => cur(acc), value);
+export const pipe = (value, ...fn) => fn.reduce((acc, cur) => cur(acc), value);
+
+class Chain {
+  constructor(value) {
+    this.value = value;
+  }
+
+  /**
+   * @returns {Chain}
+   */
+  let(callback, ...args) {
+    return new Chain(callback(this.value, ...args));
+  }
+
+  /**
+   * @returns {Chain}
+   */
+  also(callback, ...args) {
+    callback(this.value, ...args);
+    return this;
+  }
+
+  get(callback, ...args) {
+    return callback(this.value, ...args);
+  }
+}
+
+export const chain = value => new Chain(value);
---
+++ src/kotlinify.test.js
@@ -0,0 +1,27 @@
+import { chain, pipe } from './kotlinify';
+const { log } = console;
+
+const capitalize = ([first, ...rest]) => first.toUpperCase() + rest.join('');
+const cheer = (string, { times }) => string + '🍻'.repeat(times);
+const exclaim = (string, { times }) => string + '!'.repeat(times);
+
+test('a value can be chained', () => {
+  expect(
+    chain('Hello')
+      .let(capitalize)
+      .let(exclaim, { times: 2 })
+      .also(log)
+      .get(cheer, { times: 2 }),
+  ).toBe('Hello!!🍻🍻');
+});
+
+test('a value can be piped', () => {
+  expect(
+    pipe(
+      'Hello',
+      capitalize,
+      it => exclaim(it, { times: 2 }),
+      it => cheer(it, { times: 2 }),
+    ),
+  ).toBe('Hello!!🍻🍻');
+});
